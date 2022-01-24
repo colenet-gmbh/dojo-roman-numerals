d692322dc47304a9f89d7ddffc248adfec6988c1
colenet-laurin-hagemann <laurin.hagemann@colenet.de>
2022-01-24 15:11:23 +0100

    feat: pytest instead of unittest

--- python/test_app.py
+++ python/test_app.py
@@ -1,10 +1,7 @@
-import unittest
-
 from app import app
 
 
-class ApplicationTest(unittest.TestCase):
-    def test_should_get_valid_response(self):
-        with app.test_client() as client:
-            response = client.get('/')
-            self.assertEqual(response.get_data(as_text=True), '42')
+def test_should_get_valid_response():
+    with app.test_client() as client:
+        response = client.get('/')
+        assert response.get_data(as_text=True) == '42'
